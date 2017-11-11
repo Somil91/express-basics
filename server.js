b65ffb80aada852c556db600cae916aaf5ee8364
somil kumar <somil.kumar91@gmail.com>
2017-11-11 14:29:43 +0530

    Adding hbs and basic templates routes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
  const express = require('express');
+ const hbs = require('hbs');
 
+ 
  var app = express();
  
+ app.set('view engine', 'hbs');
  app.use(express.static(__dirname+'/public'));
  
  app.get('/', (req, res)=> {
@@ -12,8 +15,20 @@
     });
  });
 
+ app.get('/home', (req, res)=> {
+     res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+     });
+ });
+
+
  app.get('/about', (req, res)=> {
-     res.send('<h1>About Page</h1>');
+     res.render('about.hbs', {
+         pageTitle: 'About Page',
+         currentYear: new Date().getFullYear()
+     });
  });
 
  app.get('/bad', (req, res)=> {
